8cbc1c894155337d15e50e146bcd17c93e76db76
brianisadog <csung4@usfca.edu>
2018-04-06 20:54:46 -0700

    Environment setup

---
+++ src/EventService/Event.java
@@ -0,0 +1,143 @@
+package EventService;
+
+import com.google.gson.JsonObject;
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Thread-safe Event class.
+ */
+public class Event {
+    private final ReentrantReadWriteLock lock;
+    private final int eventId;
+    private final String eventName;
+    private final int createUserId;
+    private final int numtickets;
+    private int avail;
+    private int purchased;
+
+    /**
+     * Builder pattern to create an Event class with part of immutable data.
+     */
+    public static class EventBuilder {
+        private int eventId;
+        private String eventName;
+        private int createUserId;
+        private int numtickets;
+
+        /**
+         * Constructor of EventBuilder.
+         */
+        public EventBuilder() {}
+
+        /**
+         * EventId setter.
+         *
+         * @param eventId
+         * @return EventBuilder
+         */
+        public EventBuilder setEventId(int eventId) {
+            this.eventId = eventId;
+            return this;
+        }
+
+        /**
+         * EventName setter.
+         *
+         * @param eventName
+         * @return EventBuilder
+         */
+        public EventBuilder setEventName(String eventName) {
+            this.eventName = eventName;
+            return this;
+        }
+
+        /**
+         * CreateUserId setter.
+         *
+         * @param createUserId
+         * @return EventBuilder
+         */
+        public EventBuilder setCreateUserId(int createUserId) {
+            this.createUserId = createUserId;
+            return this;
+        }
+
+        /**
+         * Total number of tickets setter.
+         *
+         * @param numtickets
+         * @return EventBuilder
+         */
+        public EventBuilder setNumtickets(int numtickets) {
+            this.numtickets = numtickets;
+            return this;
+        }
+
+        /**
+         * Build method.
+         *
+         * @return Event
+         */
+        public Event build() {
+            return new Event(this);
+        }
+    }
+
+    /**
+     * Constructor of Event.
+     *
+     * @param eb
+     */
+    private Event(EventBuilder eb) {
+        this.lock = new ReentrantReadWriteLock();
+        this.eventId = eb.eventId;
+        this.eventName = eb.eventName;
+        this.createUserId = eb.createUserId;
+        this.numtickets = eb.numtickets;
+        this.avail = this.numtickets;
+        this.purchased = 0;
+    }
+
+    /**
+     * Synchronized purchase method to check availability and purchase.
+     *
+     * @param tickets
+     * @return boolean
+     *      - true for success, false for fail
+     */
+    public boolean purchase(int tickets) {
+        boolean result = false;
+
+        this.lock.writeLock().lock();
+        if (this.avail - tickets >= 0 && this.purchased + tickets >= 0 &&
+                (this.avail - tickets) + (this.purchased + tickets) == this.numtickets) {
+            this.avail -= tickets;
+            this.purchased += tickets;
+            result = true;
+        }
+        this.lock.writeLock().unlock();
+
+        return result;
+    }
+
+    /**
+     * Synchronized toJsonObject method to get the detail of Event with JSON format.
+     *
+     * @return JsonObject
+     *      - event detail
+     */
+    public JsonObject toJsonObject() {
+        JsonObject obj = new JsonObject();
+
+        this.lock.readLock().lock();
+        obj.addProperty("eventid", this.eventId);
+        obj.addProperty("eventname", this.eventName);
+        obj.addProperty("userid", this.createUserId);
+        obj.addProperty("avail", this.avail);
+        obj.addProperty("purchased", this.purchased);
+        this.lock.readLock().unlock();
+
+        return obj;
+    }
+}
---
+++ src/EventService/EventList.java
@@ -0,0 +1,85 @@
+package EventService;
+
+import com.google.gson.JsonArray;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+/**
+ * Thread-safe EventList data structure.
+ */
+public class EventList {
+    private final ReentrantReadWriteLock lock;
+    private final Map<Integer, Event> events;
+
+    /**
+     * Constructor of EventList.
+     */
+    public EventList() {
+        this.lock = new ReentrantReadWriteLock();
+        this.events = new HashMap<>();
+    }
+
+    /**
+     * Synchronized add method to create an Event and put it into HashMap.
+     *
+     * @param eventName
+     * @param createUserId
+     * @param numtickets
+     * @return int
+     *      - eventId, -1 for fail
+     */
+    public int add(String eventName, int createUserId, int numtickets) {
+        int eventId;
+
+        try {
+            this.lock.writeLock().lock();
+
+            eventId = this.events.size();
+            Event newEvent = new Event.EventBuilder().setEventId(eventId).setEventName(eventName)
+                    .setCreateUserId(createUserId).setNumtickets(numtickets).build();
+            this.events.put(eventId, newEvent);
+        }
+        catch (Exception ignore) {
+            eventId = -1;
+        }
+        finally {
+            this.lock.writeLock().unlock();
+        }
+
+        return eventId;
+    }
+
+    /**
+     * Synchronized get method to get Event from HashMap.
+     *
+     * @param eventId
+     * @return Event
+     */
+    public Event get(int eventId) {
+        this.lock.readLock().lock();
+        Event event = this.events.get(eventId);
+        this.lock.readLock().unlock();
+
+        return event;
+    }
+
+    /**
+     * Synchronized toString method to get a list of Events from HashMap with JSON format.
+     *
+     * @return String
+     *      - a list of Events
+     */
+    public String toString() {
+        JsonArray array = new JsonArray();
+
+        this.lock.readLock().lock();
+        for (Event event : this.events.values()) {
+            array.add(event.toJsonObject());
+        }
+        this.lock.readLock().unlock();
+
+        return array.toString();
+    }
+}
